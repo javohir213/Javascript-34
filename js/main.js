3aee64be88c4a3170f1f1b3127f97ad27b6f7381
javohir213 <ajavohir213@gmail.com>
2022-03-03 00:49:21 +0500

    Initial Mirjalol

--- js/main.js
+++ js/main.js
@@ -12,7 +12,13 @@ elForm.addEventListener("submit", function (e) {
   if (isNaN(elInput.value)) {
     elSpan.textContent = "So'z kiritish mumkin emas";
     elInput.value = "";
-  } else if (elInput.value.length == 0) {
+  }else if (elInput.value <= 0 || isNaN(elInput.value)) {
+      elSpan.textContent = "0 dan katta son kiriting";
+      elInput.value = "";
+  
+  }
+  
+  else if (elInput.value.length == 0) {
     elSpan.textContent = "Qiymat kiriting";
     elInput.value = "";
   } else if (elInput.value % 2 == 0) {
