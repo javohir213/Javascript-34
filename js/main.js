5981118d911970b2402b6085bfe5f1d56b236ff1
javohir213 <ajavohir213@gmail.com>
2022-03-03 00:30:50 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var Juft = [];
+var Toq = [];
+
+var elForm = document.querySelector("#form");
+var elInput = document.querySelector("#form__input");
+var elBtn = document.querySelector("#form__btn");
+var elSpan = document.querySelector("#resept");
+
+elForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (isNaN(elInput.value)) {
+    elSpan.textContent = "So'z kiritish mumkin emas";
+    elInput.value = "";
+  } else if (elInput.value.length == 0) {
+    elSpan.textContent = "Qiymat kiriting";
+    elInput.value = "";
+  } else if (elInput.value % 2 == 0) {
+    elSpan.textContent = "Juft son: " + elInput.value;
+    even.push(elInput.value);
+    console.log(Juft);
+    elInput.value = "";
+  } else {
+    elSpan.textContent = "Toq son: " + elInput.value;
+    odd.push(elInput.value);
+    console.log(Toq);
+    elInput.value = "";
+  }
+});
